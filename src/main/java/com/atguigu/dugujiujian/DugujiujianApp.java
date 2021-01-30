eb786c6f56d77dad701d327bc6da0ff89a73b186
420032538 <420032538@qq.com>
2021-01-30 17:31:16 +0800

    AccountB edit some thing

--- src/main/java/com/atguigu/dugujiujian/DugujiujianApp.java
+++ src/main/java/com/atguigu/dugujiujian/DugujiujianApp.java
@@ -3,5 +3,7 @@ package com.atguigu.dugujiujian;
 public class DugujiujianApp {
     public static void main(String[] args) {
        System.out.println("AAAssssssssssssssssss");
+       System.out.println("BBB");
+       System.out.println("BBB");
     }
 }
